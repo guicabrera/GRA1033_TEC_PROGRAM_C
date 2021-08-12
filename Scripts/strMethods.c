ca4bb20d7bf279cd464ddf3cbf0add859a799ca4
Guilherme Cabrera <M306833@one.merckgroup.com>
2021-08-12 20:45:06 -0300

    aula sobre lib string.h e funções de string finalizada

--- Scripts/strMethods.c
+++ Scripts/strMethods.c
@@ -8,11 +8,22 @@
 int main(void){
     setlocale(LC_ALL,"Portuguese");
 
-    char str = "meu Valor em C";
+    char str[] = "meu Valor em C";
 
     // function to return the var value in upper case
     printf(strupr(str));
     printf(strlwr(str));
 
-    
+    //comparação de strings
+    strcmp("Meu valor em C",str); //compara a string passada com a var str
+
+    //troca o valor de uma variável por outro valor
+    strcpy(str,"Batata frita");
+
+    //troca os digitos da var str de acordo com a string 
+    //passada e o número de caracteres passado
+    strncpy(str,"Troca string",3);
+
+
+
 }
